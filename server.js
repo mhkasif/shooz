2fec2eaaef1b417fa5235219465025a2b7075418
mhkasif <haseebasif97@gmail.com>
2021-03-15 13:20:50 +0500

    db attached

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const dotenv = require("dotenv");
+const mongoose  = require("mongoose");
 const app = require("./app");
 
+dotenv.config({ path: "./config.env" });
+
+
+const DB=process.env.DATABASE;
+mongoose.connect(DB,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useFindAndModify:false
+}).then(con=>{
+    console.log(con.connections)
+    console.log('db connected')
+})
 
 const PORT = 8080;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log("App running on port", PORT);
 });
