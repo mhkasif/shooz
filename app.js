55d5866f06ee5b47dd932a3deebba510b8050df9
mhkasif <haseebasif97@gmail.com>
2021-03-19 22:35:00 +0500

    testing ssh

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const AppError = require("./utils/appError");
 app.use(express.json());
 app.use("/api/v1/shoes", shoesRouter);
 app.use("/api/v1/users", userRouter);
-
-
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
 });
